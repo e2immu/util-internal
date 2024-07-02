61226de25ad4147ab566bee5acee625405ec2b72
Bart Naudts <Bart.Naudts@codelaser.io>
2024-07-02 07:56:07 +0200

    added GetSet helper

---
+++ e2immu-internal-util/src/main/java/org/e2immu/util/internal/util/GetSetHelper.java
@@ -0,0 +1,20 @@
+package org.e2immu.util.internal.util;
+
+public class GetSetHelper {
+    public static String fieldName(String methodName) {
+        String extractedName;
+        int length = methodName.length();
+        boolean set = methodName.startsWith("set");
+        boolean has = methodName.startsWith("has");
+        boolean get = methodName.startsWith("get");
+        boolean is = methodName.startsWith("is");
+        if (length >= 4 && (set || has || get) && Character.isUpperCase(methodName.charAt(3))) {
+            extractedName = methodName.substring(3);
+        } else if (length >= 3 && is && Character.isUpperCase(methodName.charAt(2))) {
+            extractedName = methodName.substring(2);
+        } else {
+            extractedName = methodName;
+        }
+        return Character.toLowerCase(extractedName.charAt(0)) + extractedName.substring(1);
+    }
+}
