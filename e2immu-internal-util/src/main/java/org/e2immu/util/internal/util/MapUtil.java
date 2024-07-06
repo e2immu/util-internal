d227a12e86ee7a617b2eb540fd04e8ca8070c93f
Bart Naudts <Bart.Naudts@codelaser.io>
2024-07-06 12:05:13 +0200

    added MapUtil

---
+++ e2immu-internal-util/src/main/java/org/e2immu/util/internal/util/MapUtil.java
@@ -0,0 +1,43 @@
+package org.e2immu.util.internal.util;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class MapUtil {
+
+
+    public static <T extends Comparable<? super T>, D extends Comparable<? super D>> int compareMaps(Map<T, D> map1, Map<T, D> map2) {
+        int c = map1.size() - map2.size();
+        if (c != 0) return c;
+        // same size
+        int differentValue = 0;
+        for (Map.Entry<T, D> e : map1.entrySet()) {
+            D dv = map2.get(e.getKey());
+            if (dv != null && differentValue == 0) {
+                // are there different values?
+                differentValue = e.getValue().compareTo(dv);
+            }
+            if (dv == null) {
+                // different keys
+                return compareKeys(map1.keySet(), map2.keySet());
+            }
+        }
+        return differentValue;
+    }
+
+    private static <T extends Comparable<? super T>> int compareKeys(Set<T> set1, Set<T> set2) {
+        TreeSet<T> treeSet1 = new TreeSet<>(set1);
+        TreeSet<T> treeSet2 = new TreeSet<>(set2);
+        Iterator<T> it1 = treeSet1.iterator();
+        Iterator<T> it2 = treeSet2.iterator();
+        while (it1.hasNext()) {
+            assert it2.hasNext();
+            int d = it1.next().compareTo(it2.next());
+            if (d != 0) return d;
+        }
+        return 0;
+    }
+
+}
