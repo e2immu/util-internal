70ae77b73da66927c241da016d7f077784988a01
Bart Naudts <Bart.Naudts@codelaser.io>
2025-06-03 15:54:49 +0200

    shortest cycle dijkstra

---
+++ e2immu-internal-graph/src/main/java/org/e2immu/util/internal/graph/op/ShortestCycleDijkstra.java
@@ -0,0 +1,63 @@
+package org.e2immu.util.internal.graph.op;
+
+import org.e2immu.util.internal.graph.G;
+import org.e2immu.util.internal.graph.V;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+public class ShortestCycleDijkstra {
+
+    public record Cycle<T>(List<V<T>> vertices, long distance) {
+    }
+
+    private record PathState<T>(V<T> vertex, long distance, List<V<T>> path, V<T> firstDestination) {
+    }
+
+    private record TwoVertices<T>(V<T> v1, V<T> v2) {
+    }
+
+    private static final int MIN_CYCLE_SIZE = 3;
+
+    public static <T> Cycle<T> shortestCycle(G<T> graph, V<T> startVertex) {
+        PriorityQueue<PathState<T>> pq = new PriorityQueue<>(Comparator.comparing(ps -> ps.distance));
+        Map<TwoVertices<T>, Long> visited = new HashMap<>();
+        Cycle<T> shortest = null;
+        for (Map.Entry<V<T>, Long> edge : graph.edges(startVertex).entrySet()) {
+            List<V<T>> initialPath = List.of(startVertex, edge.getKey());
+            PathState<T> initialState = new PathState<>(edge.getKey(), edge.getValue(), initialPath, edge.getKey());
+            pq.add(initialState);
+        }
+        while (!pq.isEmpty()) {
+            PathState<T> current = pq.poll();
+            TwoVertices<T> stateKey = new TwoVertices<>(current.vertex, current.firstDestination);
+            Long inVisited = visited.get(stateKey);
+            if (inVisited != null && inVisited <= current.distance) continue;
+            visited.put(stateKey, current.distance);
+
+            // Check if we've found a cycle back to start
+            if (startVertex.equals(current.vertex) && current.path.size() >= MIN_CYCLE_SIZE) {
+                // we've found a cycle
+                if (shortest == null || current.distance < shortest.distance) {
+                    // no need to copy the path, it is immutable
+                    shortest = new Cycle<>(current.path, current.distance);
+                }
+                // don't expand further
+                continue;
+            }
+            // Try to expand
+            for (Map.Entry<V<T>, Long> edge : graph.edges(current.vertex).entrySet()) {
+                // ensure that we don't visit one of the vertices we've already visited; exception: start
+                V<T> to = edge.getKey();
+                boolean isStart = to.equals(startVertex);
+                if (!isStart && current.path.contains(to)) continue;
+                if (isStart && current.path.size() < MIN_CYCLE_SIZE) continue; //don't bother with small cycles
+                long newDistance = current.distance + edge.getValue();
+                List<V<T>> newPath = Stream.concat(current.path.stream(), Stream.of(to)).toList();
+                PathState<T> newState = new PathState<>(to, newDistance, newPath, current.firstDestination);
+                pq.offer(newState);
+            }
+        }
+        return shortest;
+    }
+}
---
+++ e2immu-internal-graph/src/test/java/org/e2immu/util/internal/graph/op/TestShortestCycleDijkstra.java
@@ -0,0 +1,49 @@
+package org.e2immu.util.internal.graph.op;
+
+import org.e2immu.util.internal.graph.G;
+import org.e2immu.util.internal.graph.V;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class TestShortestCycleDijkstra {
+
+    @Test
+    public void test() {
+        // 0 ->1-> 1, 0 ->4-> 2, 0 ->5-> 3
+        // 1 ->2-> 2
+        // 2 ->1-> 3
+        // 3 ->1-> 4
+        // 4 ->4-> 1
+
+        // so 0 does not participate in the cycle, but the other nodes do
+
+        Map<Integer, Map<Integer, Long>> edges = Map.of(
+                0, Map.of(1, 1L, 2, 4L, 3, 5L),
+                1, Map.of(2, 2L),
+                2, Map.of(3, 1L),
+                3, Map.of(4, 1L),
+                4, Map.of(1, 4L)
+        );
+        G.Builder<Integer> gb = new G.Builder<>(Long::sum);
+        edges.forEach((from, m) ->
+                m.forEach((to, d) -> gb.mergeEdge(from, to, d)));
+        G<Integer> graph = gb.build();
+        assertEquals("""
+                0->1->1
+                0->4->2
+                0->5->3
+                1->2->2
+                2->1->3
+                3->1->4
+                4->4->1\
+                """, graph.toString("\n"));
+        ShortestCycleDijkstra.Cycle<Integer> cycle1 = ShortestCycleDijkstra.shortestCycle(graph, new V<>(1));
+        assertEquals("Cycle[vertices=[1, 2, 3, 4, 1], distance=8]", cycle1.toString());
+        ShortestCycleDijkstra.Cycle<Integer> cycle0 = ShortestCycleDijkstra.shortestCycle(graph, new V<>(0));
+        assertNull(cycle0);
+    }
+}
