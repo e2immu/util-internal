dd512cdba8b257a09d11eacbd00500cf253a2bd3
Bart Naudts <Bart.Naudts@codelaser.io>
2025-06-04 07:40:33 +0200

    code for dumping a graph to GML

--- e2immu-internal-graph/src/main/java/org/e2immu/util/internal/graph/analyser/TypeGraphIO.java
+++ e2immu-internal-graph/src/main/java/org/e2immu/util/internal/graph/analyser/TypeGraphIO.java
@@ -1,15 +1,25 @@
 package org.e2immu.util.internal.graph.analyser;
 
+import org.e2immu.util.internal.graph.G;
+import org.e2immu.util.internal.graph.V;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.builder.GraphTypeBuilder;
+import org.jgrapht.nio.Attribute;
+import org.jgrapht.nio.DefaultAttribute;
+import org.jgrapht.nio.gml.GmlExporter;
 import org.jgrapht.nio.gml.GmlImporter;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.Function;
 
 public class TypeGraphIO {
 
@@ -32,7 +42,7 @@ public class TypeGraphIO {
                                    Graph<Node, DefaultWeightedEdge> graph) throws IOException {
         GmlImporter<Node, DefaultWeightedEdge> importer = new GmlImporter<>();
         try (InputStreamReader reader = new InputStreamReader(Objects.requireNonNull(inputStream),
-                     StandardCharsets.UTF_8)) {
+                StandardCharsets.UTF_8)) {
             importer.setVertexFactory(Node::new);
             importer.addVertexAttributeConsumer((pair, attribute) -> {
                 if ("label".equals(pair.getSecond())) {
@@ -80,5 +90,55 @@ public class TypeGraphIO {
                 .edgeClass(DefaultWeightedEdge.class)
                 .weighted(true)
                 .buildGraph();
+    }
+
+    public static <T> void dumpGraph(File file, G<T> typeGraph) throws IOException {
+        Graph<T, DefaultWeightedEdge> graph =
+                GraphTypeBuilder.<T, DefaultWeightedEdge>directed()
+                        .allowingMultipleEdges(false)
+                        .allowingSelfLoops(true)
+                        .edgeClass(DefaultWeightedEdge.class)
+                        .weighted(true)
+                        .buildGraph();
+        for (Map.Entry<V<T>, Map<V<T>, Long>> entry : typeGraph.edges()) {
+            T typeInfo = entry.getKey().t();
+            if (!graph.containsVertex(typeInfo)) graph.addVertex(typeInfo);
+            for (Map.Entry<V<T>, Long> e2 : entry.getValue().entrySet()) {
+                T target = e2.getKey().t();
+                if (!graph.containsVertex(target)) graph.addVertex(target);
+                DefaultWeightedEdge e = graph.addEdge(typeInfo, target);
+                graph.setEdgeWeight(e, e2.getValue());
+            }
+        }
+        Function<T, Map<String, Attribute>> vertexAttributeProvider = (v) -> {
+            Map<String, Attribute> map = new LinkedHashMap<>();
+            map.put("label", DefaultAttribute.createAttribute(v.toString()));
+            V<T> vertex = typeGraph.vertex(v);
+            Map<V<T>, Long> edges = typeGraph.edges(vertex);
+            // weight is the sum of the outgoing edge weights
+            long sum = edges == null ? 0L : edges.values().stream().mapToLong(x -> x).sum();
+            map.put("weight", DefaultAttribute.createAttribute(sum));
+            return map;
+        };
+        exportWeightedGraph(graph, vertexAttributeProvider, file);
+    }
+
+    private static <T> void exportWeightedGraph(Graph<T, DefaultWeightedEdge> graph,
+                                                Function<T, Map<String, Attribute>> vertexAttributeProvider,
+                                                File file) throws IOException {
+        GmlExporter<T, DefaultWeightedEdge> exporter = new GmlExporter<>();
+        exporter.setParameter(GmlExporter.Parameter.EXPORT_VERTEX_LABELS, true);
+        exporter.setParameter(GmlExporter.Parameter.EXPORT_CUSTOM_VERTEX_ATTRIBUTES, true);
+        exporter.setVertexAttributeProvider(vertexAttributeProvider);
+        exporter.setParameter(GmlExporter.Parameter.EXPORT_EDGE_WEIGHTS, false);
+        exporter.setParameter(GmlExporter.Parameter.EXPORT_EDGE_LABELS, false);
+        exporter.setParameter(GmlExporter.Parameter.EXPORT_CUSTOM_EDGE_ATTRIBUTES, true);
+        exporter.setEdgeAttributeProvider(e -> {
+            Map<String, Attribute> map = new LinkedHashMap<>();
+            int w = (int) graph.getEdgeWeight(e);
+            map.put("weight", DefaultAttribute.createAttribute(w));
+            return map;
+        });
+        exporter.exportGraph(graph, file);
     }
 }
--- e2immu-internal-graph/src/test/java/org/e2immu/util/internal/graph/op/TestShortestCycleDijkstra.java
+++ e2immu-internal-graph/src/test/java/org/e2immu/util/internal/graph/op/TestShortestCycleDijkstra.java
@@ -2,8 +2,11 @@ package org.e2immu.util.internal.graph.op;
 
 import org.e2immu.util.internal.graph.G;
 import org.e2immu.util.internal.graph.V;
+import org.e2immu.util.internal.graph.analyser.TypeGraphIO;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 public class TestShortestCycleDijkstra {
 
     @Test
-    public void test() {
+    public void test() throws IOException {
         // 0 ->1-> 1, 0 ->4-> 2, 0 ->5-> 3
         // 1 ->2-> 2
         // 2 ->1-> 3
@@ -45,5 +48,15 @@ public class TestShortestCycleDijkstra {
         assertEquals("Cycle[vertices=[1, 2, 3, 4, 1], distance=8]", cycle1.toString());
         ShortestCycleDijkstra.Cycle<Integer> cycle0 = ShortestCycleDijkstra.shortestCycle(graph, new V<>(0));
         assertNull(cycle0);
+
+        TypeGraphIO.dumpGraph(new File("build/cycle.gml"), graph);
+
+        Linearize.Result<Integer> res = Linearize.linearize(graph, Linearize.LinearizationMode.ALL);
+        assertEquals(1, res.remainingCycles().size());
+        Cycle<Integer> cycle = res.remainingCycles().iterator().next();
+        assertEquals(4, cycle.size());
+        assertEquals(1, res.attachedToCycles().size());
+        assertEquals("[1, 2, 3, 4]", cycle.vertices().stream().map(Object::toString).sorted().toList().toString());
+        assertEquals("[0]", res.attachedToCycles().sortedStream(Integer::compareTo).toList().toString());
     }
 }
