70de1e04c336ebd6823ed67f04c5f6975d41f577
Bart Naudts <Bart.Naudts@codelaser.io>
2025-06-06 10:58:44 +0200

    stub class

---
+++ e2immu-internal-graph/src/main/java/org/e2immu/util/internal/graph/op/SimpleDijkstraShortestPath.java
@@ -0,0 +1,12 @@
+package org.e2immu.util.internal.graph.op;
+
+import org.e2immu.util.internal.graph.G;
+
+import java.util.Map;
+
+public class SimpleDijkstraShortestPath {
+
+   public static <T> Map<T, Long> shortestPath(G<T> graph, T startIndex) {
+        throw new UnsupportedOperationException("NYI");
+    }
+}
