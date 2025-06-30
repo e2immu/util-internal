d4904b739b1a700975d2fd323c11ac053f1d5d7d
Bart Naudts <Bart.Naudts@codelaser.io>
2025-06-30 09:09:50 +0200

    added module-info.java

---
+++ e2immu-internal-graph/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module org.e2immu.util.internal.graph {
+    requires org.slf4j;
+    requires org.jgrapht.core;
+    requires org.jgrapht.io;
+}
---
+++ e2immu-internal-util/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module org.e2immu.util.internal.util {
+    requires org.e2immu.util.external.support;
+    requires org.slf4j;
+    exports org.e2immu.util.internal.util;
+}
