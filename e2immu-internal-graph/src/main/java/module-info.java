401242b4af97a4b868bdc7b5e3d8ceafde327bcb
Bart Naudts <Bart.Naudts@codelaser.io>
2025-06-30 09:56:09 +0200

    expose more packages in module-info.java

--- e2immu-internal-graph/src/main/java/module-info.java
+++ e2immu-internal-graph/src/main/java/module-info.java
@@ -2,4 +2,9 @@ module org.e2immu.util.internal.graph {
     requires org.slf4j;
     requires org.jgrapht.core;
     requires org.jgrapht.io;
+
+    exports org.e2immu.util.internal.graph;
+    exports org.e2immu.util.internal.graph.op;
+    exports org.e2immu.util.internal.graph.analyser;
+    exports org.e2immu.util.internal.graph.util;
 }
